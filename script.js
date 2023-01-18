ea4cd064d594e517553485d6dfa6de50d173b971
Vinícius Souza <tradutorvinicius@gmail.com>
2023-01-17 21:19:58 -0300

    Resolve requisitos 18 e 19

--- script.js
+++ script.js
@@ -2,11 +2,21 @@
 const btn = document.querySelector('#btn');
 const password = document.querySelector('#password');
 const email = document.querySelector('#email');
+const submitBtn = document.querySelector('#submit-btn');
+const checkAgreement = document.querySelector('#agreement');
 
 btn.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
+  }
+});
+
+checkAgreement.addEventListener('click', () => {
+  if (checkAgreement.checked === false) {
+    submitBtn.disabled = true;
+  } else {
+    submitBtn.disabled = false;
   }
 });
