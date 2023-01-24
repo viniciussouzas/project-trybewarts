e3b1757277baa83163da294c4c27757aedbb3a81
Vinícius Souza <tradutorvinicius@gmail.com>
2023-01-24 20:49:36 -0300

    Resolve requisito 21, e termina de estilizar página do projeto

--- script.js
+++ script.js
@@ -35,14 +35,112 @@ desc.addEventListener('input', (e) => {
 
 const capturaName = () => {
   const name = document.querySelector('#input-name');
-  const lastName = document.querySelector('input-lastname');
-  const fullName = document.createElement('p');
-  fullName.innerHTML = `Nome: ${name.value} ${lastName.value}`;
-  return fullName;
+  const lastName = document.querySelector('#input-lastname');
+  const nameForm = document.createElement('p');
+  nameForm.innerHTML = `Nome: ${name.value} ${lastName.value}`;
+
+  newForm.appendChild(nameForm);
 };
 
-submitBtn.addEventListener('click', (e) => {
-  e.preventDefault();
+const capturaEmail = () => {
+  const emailCaptured = document.querySelector('#input-email');
+  const emailForm = document.createElement('p');
+  emailForm.innerHTML = `Email: ${emailCaptured.value}`;
+
+  newForm.appendChild(emailForm);
+};
+
+const capturaHouse = () => {
+  let houseSelect = '';
+
+  const housesCaptured = document.querySelectorAll('.houses');
+
+  for (let index = 0; index < housesCaptured.length; index += 1) {
+    if (housesCaptured[index].selected) {
+      houseSelect = housesCaptured[index].innerHTML;
+    }
+  }
+
+  const houseForm = document.createElement('p');
+  houseForm.innerHTML = `Casa: ${houseSelect}`;
+
+  newForm.appendChild(houseForm);
+};
+
+const capturaFamily = () => {
+  let familySelect = '';
+
+  const familyCaptured = document.querySelectorAll('.families');
+
+  for (let index = 0; index < familyCaptured.length; index += 1) {
+    if (familyCaptured[index].checked) {
+      familySelect = familyCaptured[index].value;
+    }
+  }
+  const familyForm = document.createElement('p');
+  familyForm.innerHTML = `Família: ${familySelect}`;
+
+  newForm.appendChild(familyForm);
+};
+
+const capturaSubjects = () => {
+  const subjectSelect = [];
+
+  const subjectCaptured = document.querySelectorAll('.subject');
+
+  for (let index = 0; index < subjectCaptured.length; index += 1) {
+    if (subjectCaptured[index].checked) {
+      subjectSelect.push(subjectCaptured[index].value);
+    }
+  }
+  const subjectForm = document.createElement('p');
+  subjectForm.innerHTML = `Matérias: ${subjectSelect.join(', ')}`;
+
+  newForm.appendChild(subjectForm);
+};
+
+const capturaRate = () => {
+  let rateSelect = '';
+
+  const rateCaptured = document.querySelectorAll('.rates');
+
+  for (let index = 0; index < rateCaptured.length; index += 1) {
+    if (rateCaptured[index].checked) {
+      rateSelect = rateCaptured[index].value;
+    }
+  }
+  const rateForm = document.createElement('p');
+  rateForm.innerHTML = `Avaliação: ${rateSelect}`;
+
+  newForm.appendChild(rateForm);
+};
+
+const capturaComment = () => {
+  const commentCaptured = document.querySelector('#textarea');
+  const commentForm = document.createElement('p');
+  commentForm.innerHTML = `Observações: ${commentCaptured.value}`;
+
+  newForm.appendChild(commentForm);
+};
+
+const hidePage = () => {
   evaluationForm.style.display = 'none';
-  newForm.appendChild(capturaName());
+  newForm.style.width = '675px';
+  newForm.style.textAlign = 'left';
+  newForm.style.marginLeft = '5%';
+  newForm.style.marginTop = '115px';
+  newForm.style.marginBottom = '115px';
+  newForm.style.padding = '8px 20px';
+};
+
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  hidePage();
+  capturaName();
+  capturaEmail();
+  capturaHouse();
+  capturaFamily();
+  capturaSubjects();
+  capturaRate();
+  capturaComment();
 });
